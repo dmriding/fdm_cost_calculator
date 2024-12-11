187cb431a7b213502c1fa357b3ebbf35f07f31d2
DRiding <davidriding20@gmail.com>
2024-12-11 21:44:40 +0200

    allow resizing

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,14 @@ mod ui;
 use crate::ui::{CalculatorUI, load_logo};
 
 fn main() -> Result<(), eframe::Error> {
-    let options = eframe::NativeOptions::default();
+    let options = eframe::NativeOptions {
+        vsync: true,               // Enable vertical sync
+        multisampling: 1,          // Use default multisampling (anti-aliasing)
+        depth_buffer: 0,           // Disable depth buffer (2D app)
+        stencil_buffer: 0,         // Disable stencil buffer (2D app)
+        ..Default::default()       // Use other default options
+    };
+
     eframe::run_native(
         "FDM Cost Calculator",
         options,
